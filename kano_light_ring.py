89141980385ba13c5e56371523b6e110b7d50579
Jersevs <jersevemmanuel@gmail.com>
2025-07-11 11:45:35 -0700

    kano(style): added colors to display, edited logic

--- kano_light_ring.py
+++ kano_light_ring.py
@@ -70,7 +70,7 @@ LED_COUNT = 10        # Change this to match the number of LEDs in your ring (10
 LED_PIN = 18          # GPIO pin (18 works best for PWM on Pi)
 LED_FREQ_HZ = 800000  # LED signal frequency
 LED_DMA = 10          # DMA channel to use
-LED_BRIGHTNESS = 255  # Full brightness (0-255)
+LED_BRIGHTNESS = 15  # Full brightness (0-255)
 LED_INVERT = False    # Needed if using a logic level converter (usually False)
 LED_CHANNEL = 0       # Channel for PWM
 
@@ -78,16 +78,44 @@ LED_CHANNEL = 0       # Channel for PWM
 strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
 
+gradientColors = [
+    (50, 200, 200),
+    (68, 213, 168),
+    (87, 227, 137),
+    (106, 240, 106),
+    (125, 220, 75),
+    (20, 20, 255),
+    (10, 40, 244),
+    (20, 80, 233),
+    (30, 120, 222),
+    (40, 160, 211)
+]
+
 # colors (R, B, G)
 RED = Color(255, 0, 0)
 ORANGE = Color(255, 100, 0)
 YELLOW = Color(255, 255, 0)
 GREEN = Color(0, 255, 0)
-BLUE = Color(0, 255, 0)
+BLUE = Color(0, 0, 255)
 PURPLE = Color(100, 0, 255)
 PINK = Color(255, 0, 255)
 WHITE = Color(255, 255, 255)
 GRAY = Color(10, 10, 10)
+NEPEACH = Color(200, 50, 50)
+NEPEACHDIM = Color(40, 10, 10)
+NEORANGE2 = Color(200, 75, 10)
+NEYELLOW = Color(175, 100, 10)
+NEYELLOWDIM = Color(35, 20, 2)
+NEYELLOW2 = Color(150, 125, 10)
+NERED = Color(200, 25, 25)
+NEMINT2 = Color(125,254,75)
+NEMINT2DIM = Color(25, 51, 15)
+NETEAL = Color(50,200,200)
+NEPURPLE = Color(75,50,250)
+NEPURPLE2 = Color(100, 50, 175)
+NEPURPLE2DIM = Color(20, 10, 35)
+NEPURPLE3 = Color(125, 50, 150)
+OFF = Color(0, 0, 0)
 
 def convertLightNums(num): # 0th light is top, 1st is next one clockwise
     if num <= 9 and num >= 5:
@@ -116,15 +144,19 @@ def solidColor(strip, color):
         strip.setPixelColor(i, color)
     strip.show()
 
-def turnOnLightsInRange(rangenum, color):
+def turnOnLightsWithGradient(rangenum):
     for light in range(rangenum):
-        strip.setPixelColor(convertLightNums(light), color)
+        index = convertLightNums(light)
+        r, g, b = gradientColors[index]
+        strip.setPixelColor(index, Color(r, g, b))
+    strip.show()
     return
 
 def minutes_to_light():
 
     current_minutes = tf.get_minutes()
-    # current_minutes = 50
+
+    # current_minutes = 58
     # debugging ^^
 
     # range of minutes that will turn on those lights
@@ -138,27 +170,32 @@ def minutes_to_light():
         rangeMin_minutes -= 6
         rangeMax_minutes -= 6
         range_of_lights_to_turn_on -= 1
-        print(f'{rangeMin_minutes}, {rangeMax_minutes}, {range_of_lights_to_turn_on}')
-    
+        # print(f'{rangeMin_minutes}, {rangeMax_minutes}, {range_of_lights_to_turn_on}')
 
     # once the while loop is broken...
-    turnOnLightsInRange(range_of_lights_to_turn_on, WHITE)
+    turnOnLightsWithGradient(range_of_lights_to_turn_on)
 
 
 def hourbinary_to_light():
     binary = tf.hour_to_binary()
     binary_str = str(binary).zfill(4)  # Ensure the binary string is 4 digits
     led_positions = [2, 1, 0, 9]  # Corresponding LED positions for each binary digit
-    for i, digit in enumerate(binary_str):
-        if digit == '1':
-            strip.setPixelColor(led_positions[i], WHITE)
-        else:
-            strip.setPixelColor(led_positions[i], GRAY)
     emmage = tf.am_or_pm()
+    for i, digit in enumerate(binary_str):
+        if emmage == 'AM':
+            if digit == '1':
+                strip.setPixelColor(led_positions[i], NEYELLOW2)
+            else:   
+                strip.setPixelColor(led_positions[i], NEYELLOWDIM)
+        if emmage == 'PM':
+            if digit == '1':
+                strip.setPixelColor(led_positions[i], NEPURPLE3)
+            else:
+                strip.setPixelColor(led_positions[i], NEPURPLE2DIM)
     if emmage == 'AM':
-        strip.setPixelColor(8, YELLOW)
+        strip.setPixelColor(8, NEORANGE2)
     else:
-        strip.setPixelColor(8, PURPLE)
+        strip.setPixelColor(8, NEPURPLE)
     return
                 
 def setAllFakesGray():
